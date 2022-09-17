edf2d8b1a4cbd34a07d70600fea483b672ae304c
munenepeter <peternjega7@gmail.com>
2022-09-17 17:08:40 +0300

    Dont know what I'm doing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Test";
+$pearls = array_unique(explode(",",file_get_contents('pearls.txt')));
+
+$pearls = array_map(function ($pearl){
+  return trim($pearl);
+}, $pearls);
+
+echo "<pre>";
+var_dump($pearls);
+
